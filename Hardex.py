4c5b75c54a1099d888bc9582d763edb302434845
Rahul Polisetti <rahulpolisetti@gmail.com>
2018-06-28 17:57:12 +0530

    Update and rename test.py to Hardex.py
    removed usage of MATLAB code for shadow removal.

--- Hardex.py
+++ Hardex.py
@@ -2,23 +2,17 @@ import scipy
 import numpy as np
 import warnings
 import os
-import matplotlib.pyplot as plt
 import progressbar
 
-from oct2py import octave
-from skimage.filters import median, sobel_h
+from skimage.filters import median
 from skimage import io
 from skimage.feature import canny
 from skimage.transform import rotate
 from skimage.morphology import disk
-from scipy.signal import savgol_filter
-from scipy import ndimage as ndi
-from skimage.exposure import equalize_hist
 
 
+# Savitzky-Golay filter
 def sgolay2d(z, window_size, order, derivative=None):
-    """
-    """
     # number of terms in the polynomial expression
     n_terms = (order + 1) * (order + 2) / 2.0
 
@@ -111,80 +105,65 @@ def sgolay2d(z, window_size, order, derivative=None):
 
 
 def remove(num, folder, fit, out_folder):
-    name = folder + "/P" + str(num) + ".tif"
-    input1 = io.imread(name)
-    input1_copy = input1
-    original = octave.shadow_remove(name)
-    denoised = median(original, disk(20))
-    edges = sobel_h(denoised)
-    sgol = sgolay2d(edges, 15, 4)
-    height, width = sgol.shape
-    mask = sgol < 0.019
-    sgol[mask] = 0
-    mask = sgol > 0.019
-    sgol[mask] = 1
-    sgol[800:, :] = 0
+    original = io.imread(folder + "/P" + str(num) + ".tif")
 
-    x = [0] * width
+    denoised = median(original, disk(10))
 
-    for i in range(width):
-        for j in range(height - 1, 0, -1):
-            if sgol[j, i] == 1:
-                x[i] = j
-                break
-    z = np.polyfit(range(0, 1024), x, fit)
-    f = np.poly1d(z)
-    x_new = np.linspace(0, 1023, 1024)
-    y_new = f(x_new)
-    clear = max(y_new)
-
-    sgol[int(clear):, :] = 0
-
-    x = [0] * width
-
-    for i in range(width):
-        for j in range(height - 1, 0, -1):
-            if sgol[j, i] == 1:
-                x[i] = j
-                break
-    z = np.polyfit(range(0, 1024), x, fit)
-    f = np.poly1d(z)
-    x_new = np.linspace(0, 1023, 1024)
-    y_new = f(x_new)
-
-    for i in range(width - 1):
-        if any(t == 1 for t in sgol[x[i] - 16:x[i] + 16, i + 1]):
-            pass
-        else:
-            x[i + 1] = int(y_new[i + 1])
-
-    for i in range(width):
-        input1[x[i] - 15:, i] = 0
-
-    denoised = median(input1, disk(10))
     edges = canny(denoised, sigma=0.1)
+    
 
+    # Savitzky-Golay filter
     sgol = sgolay2d(edges, 15, 4)
     mask = sgol < 0.1
     sgol[mask] = 0
     mask = sgol > 0.1
     sgol[mask] = 1
 
-    for i in range(0, width):
-        for j in range(0, height):
-            input1[j, i] = 0
-            if sgol[j, i] == 1:
-                input1[j:j + 50, i] = 0
+    rotated = rotate(sgol, 270, resize=True)
+    height, width = rotated.shape
+    x = [0] * 1024
+
+    for i in range(0, height):
+        for j in range(0, width):
+            if rotated[i, j] == 1:
+                x[i] = j
                 break
 
-    output = equalize_hist(input1_copy)
-    # output = median(output, disk(5))
-    # mask = input1 == 0
-    # output[mask] = 0
-    # mask = output < 170
-    # output[mask] = 0
-    io.imsave(out_folder + "/" + str(num) + ".bmp", output)
+    z = np.polyfit(range(0, 1024), x, fit)
+    f = np.poly1d(z)
+    x_new = np.linspace(0, 1023, 1024)
+    y_new = f(x_new)
 
+    original_rot = rotate(original, 270, resize=True)
+    height, width = original_rot.shape
+
+    line = [0] * 1024
+    for i in range(height):
+        if y_new[i] > x[i]:
+            line[i] = y_new[i]
+        else:
+            line[i] = x[i]
+
+    for i in range(0, height):
+        for j in range(0, width):
+            original_rot[i, j] = 0
+            if int(line[i] + 30) == j:
+                break
+
+
+    for i in range(0, height):
+        for j in range(width - 1, 0, -1):
+            original_rot[i, j] = 0
+            if rotated[i, j] == 1:
+                original_rot[i, j - 50:j] = 0
+                break
+
+    output = rotate(original_rot, 90, resize=True)
+    mask = output < 0.700
+    output[mask] = 0
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        io.imsave(out_folder + "/" + str(num) + ".bmp", output)
 
 def incrementing_bar():
     folder = input("enter input folder name: ")
@@ -198,10 +177,9 @@ def incrementing_bar():
         progressbar.Bar(),
     ], max_value=256).start()
     for i in range(num, num + 256):
-        with warnings.catch_warnings():
-            warnings.simplefilter("ignore")
-            remove(i, folder, fit, out_folder)
+        remove(i, folder, fit, out_folder)
         bar.update(i - num + 1)
     bar.finish()
+
 
 incrementing_bar()
